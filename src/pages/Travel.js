d17186acadd0028e52eb2d78cd348827c6cd7fa7
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-01-10 09:59:14 -0500

    added details to the travel page

--- src/pages/Travel.js
+++ src/pages/Travel.js
@@ -12,6 +12,28 @@ export class Travel extends Component {
         <section className="image-container">
           <img src={saveTheDateImg2} />
         </section>
+        <section className="wedding-details">
+          <h1>Airport</h1>
+          <h3>Baltimore-Washington International Airport</h3>
+          <section className="reception-details">
+            <h1>Room Block #1</h1>
+            <h3>
+              Click{' '}
+              <a href="https://book.passkey.com/event/50050495/owner/247310/home?utm_campaign=285752590" targe="_blank">
+                {' '}
+                here{' '}
+              </a>{' '}
+              to book (one night minimum at $229 plus tax)
+            </h3>
+            <p>
+              The Westin Annapolis
+              <br />
+              100 Westgate Circle
+              <br />
+              Annapolis, MD 21401
+            </p>
+          </section>
+        </section>
       </div>
     )
   }
